8b66b0321391ed1bf457bf40e0930991260fc455
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-07-27 23:31:23 +0200

    add on

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Watchlist } from "./components/Watchlist";
+import { Watched } from "./components/Watched";
+import { Add } from "./components/Add";
+import "./App.css";
+import "./lib/font-awesome/css/all.min.css";
+
+import { GlobalProvider } from "./context/GlobalState";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <Watchlist />
+          </Route>
+          <Route path="/add">
+            <Add />
+          </Route>
+          <Route path="/watched">
+            <Watched />
+          </Route>
+        </Switch>
+      </Router>
+    </GlobalProvider>
+  );
+}
+
+export default App;
